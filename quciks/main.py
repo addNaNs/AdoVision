61a92a07adbeb3b925b5707d884c858e55da476c
addNaNs <adi.saban.1999@gmail.com>
2020-04-16 20:53:04 +0200

    added image loading

--- quciks/main.py
+++ quciks/main.py
@@ -1,7 +1,21 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import seaborn as sns
 import tensorflow as tf
 
-print('Valja')
+digit_dirs = [os.path.join('../data/japanesehandwrittendigits/0'+ str(i)) for i in range(10)]
+
+print('total_images = ' + str(len(os.listdir(digit_dirs[0]))))
+
+digit_files = [os.listdir(digit_dirs[i]) for i in range(10)]
+print(digit_files[0][:10])
+
+for i, img_path in enumerate([os.path.join(digit_dirs[6], f_name) for f_name in digit_files[6][0:5]]):
+    print(img_path)
+    img = mpimg.imread(img_path)
+    plt.imshow(img)
+    plt.axis('Off')
+    plt.show()
