ba8ae03f7bb8fb29f66d6f6542441d675ef852be
addNaNs <adi.saban.1999@gmail.com>
2020-04-17 17:01:31 +0200

    added image loading, and tested that it shows

--- quciks/main.py
+++ quciks/main.py
@@ -13,9 +13,12 @@ print('total_images = ' + str(len(os.listdir(digit_dirs[0]))))
 digit_files = [os.listdir(digit_dirs[i]) for i in range(10)]
 print(digit_files[0][:10])
 
-for i, img_path in enumerate([os.path.join(digit_dirs[6], f_name) for f_name in digit_files[6][0:5]]):
-    print(img_path)
-    img = mpimg.imread(img_path)
-    plt.imshow(img)
-    plt.axis('Off')
-    plt.show()
+
+want_to_print = False
+if want_to_print:
+    for i, img_path in enumerate([os.path.join(digit_dirs[6], f_name) for f_name in digit_files[6][0:5]]):
+        print(img_path)
+        img = mpimg.imread(img_path)
+        plt.imshow(img)
+        plt.axis('Off')
+        plt.show()
